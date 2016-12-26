8a9fc6536ae77ae4d87713fb4df423197750293a
Raymond Usbal <raymond@philippinedev.com>
2016-12-26 11:51:58 +0800

    display main featured image if blog archive page

--- wp-content/themes/gk-portfolio/content-header.php
+++ wp-content/themes/gk-portfolio/content-header.php
@@ -20,16 +20,19 @@
 <header class="entry-header<?php if(is_singular() && is_sticky()) :?> sticky<?php endif; ?><?php if(get_theme_mod('portfolio_full_width_images', '1') == '1') : ?> full-width-image<?php endif; ?><?php if(get_theme_mod('portfolio_post_show_title', '1') == '0') : ?> no-title<?php endif; ?>" data-url="<?php echo get_permalink(); ?>">
 	<?php if(!is_singular() || get_theme_mod('portfolio_post_show_featured_image', '1') == '1') : ?>
 		<?php if (has_post_thumbnail() && !post_password_required()) : ?>			
-			<?php //the_post_thumbnail(is_singular() ? 'full' : 'gk-portfolio-size'); ?>
-            <?php
-            if( class_exists('Dynamic_Featured_Image') ) {
-                global $dynamic_featured_image;
-                $featured_images = $dynamic_featured_image->get_featured_images( );
+			<?php
+            if (is_home()):
+                the_post_thumbnail(is_singular() ? 'full' : 'gk-portfolio-size');
+            else:
+                if( class_exists('Dynamic_Featured_Image') ) {
+                    global $dynamic_featured_image;
+                    $featured_images = $dynamic_featured_image->get_featured_images();
 
-                foreach ($featured_images as $feature_image) {
-                    echo '<img src="' . $feature_image['full'] . '">';
+                    foreach ($featured_images as $feature_image) {
+                        echo '<img src="' . $feature_image['full'] . '">';
+                    }
                 }
-            }
+            endif;
             ?>
         <?php elseif($video_code) : ?>
 			<div class="video-wrapper">
